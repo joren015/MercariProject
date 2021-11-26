b20b1a0c908e71b73115f96bd8c82130d3c6da7c
alexjorenby <alexjorenby@gmail.com>
2021-11-26 12:39:01 -0600

    Using python3 command instead of python

--- setup_local_workspace.sh
+++ setup_local_workspace.sh
@@ -5,16 +5,16 @@ sudo apt-get update \
     && sudo apt-get install -y p7zip-full unzip
 
 # Install python virtual environment
-python -m pip install virtualenv==20.10.0
+python3 -m pip install virtualenv==20.10.0
 
 # Createand activate virtual environment
 if [ ! -d ".venv" ]
 then
-    python -m venv .venv
+    python3 -m venv .venv
 fi
 source ./.venv/bin/activate
 
 # Install required packages
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 ./download_dataset.sh
