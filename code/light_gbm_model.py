Initial pass at category model. Fixing data leak issue found in NN and light_gbm models.

code/light_gbm_model.py
@@ -1,6 +1,5 @@
-import gc
 import pickle
-from os.path import exists
+from uuid import uuid4
 
 import numpy as np
 import pandas as pd
@@ -18,28 +17,14 @@ pd.set_option('max_colwidth', 200)
 
 
 class LightGBMModel(BaseEstimator, RegressorMixin):
-    def __init__(self, experiment="LightGBM", quick_preprocess=False, **kwarg):
+    def __init__(self, experiment="LightGBM", **kwarg):
         self.experiment = experiment
         self.model = LGBMRegressor(n_estimators=200,
                                    learning_rate=0.5,
                                    num_leaves=125,
                                    random_state=42)
         self.vectorizers = {}
-        for col in [
-                "name", "item_description", "brand_name", "item_condition_id",
-                "shipping", "cat_dae", "cat_jung", "cat_so"
-        ]:
-            if not exists(
-                    "data/light_gbm/{}_vectorizer/vectorizer.pkl".format(col)):
-                if quick_preprocess:
-                    self.preprocess(10000)
-                else:
-                    self.preprocess()
-
-            self.vectorizers[
-                col] = "data/light_gbm/{}_vectorizer/vectorizer.pkl".format(
-                    col)
-
+        self.vectorizer_guid = str(uuid4()).replace('-', '_')
         self.eval_metric = make_scorer(self.score, greater_is_better=False)
         self.metrics = {
             "Root mean squared log error": self.eval_metric,
@@ -54,6 +39,12 @@ class LightGBMModel(BaseEstimator, RegressorMixin):
         }
 
     def fit(self, X, y):
+        self.model = LGBMRegressor(n_estimators=200,
+                                   learning_rate=0.5,
+                                   num_leaves=125,
+                                   random_state=42)
+        self.vectorizer_guid = str(uuid4()).replace('-', '_')
+        self.preprocess(X)
         X = self.apply_preprocessing(X)
         y = np.log1p(y)
         self.model.fit(X, y)
@@ -69,29 +60,35 @@ class LightGBMModel(BaseEstimator, RegressorMixin):
     def get_params(self, deep=True):
         return self.model.get_params(deep=deep)
 
-    def preprocess(self, nrows=-1):
-        if nrows > 0:
-            mercari_df = pd.read_csv('data/train.tsv', sep='\t', nrows=nrows)
-        else:
-            mercari_df = pd.read_csv('data/train.tsv', sep='\t')
+    def preprocess(self, X):
+        # if nrows > 0:
+        #     mercari_df = pd.read_csv('data/train.tsv', sep='\t', nrows=nrows)
+        # else:
+        #     mercari_df = pd.read_csv('data/train.tsv', sep='\t')
 
-        mercari_df = self.common_preprocessing(mercari_df)
-        gc.collect()
+        df = self.common_preprocessing(X)
 
         print("Vectorizing name")
         # Convert "name" with feature vectorization
-        self.vectorizers["name"] = fit_and_save_vectorizer(
-            mercari_df["name"], CountVectorizer(max_features=30000),
-            "data/light_gbm/name_vectorizer")
+        fit_and_save_vectorizer(
+            df["name"], CountVectorizer(max_features=30000),
+            "data/light_gbm/{}/name_vectorizer".format(self.vectorizer_guid))
+        self.vectorizers[
+            "name"] = "data/light_gbm/{}/name_vectorizer/vectorizer.pkl".format(
+                self.vectorizer_guid)
 
         print("Vectorizing item_description")
         # Convert "item_description" with feature vectorization
-        self.vectorizers["item_description"] = fit_and_save_vectorizer(
-            mercari_df['item_description'],
+        fit_and_save_vectorizer(
+            df['item_description'],
             TfidfVectorizer(max_features=50000,
                             ngram_range=(1, 3),
                             stop_words='english'),
-            "data/light_gbm/item_description_vectorizer")
+            "data/light_gbm/{}/item_description_vectorizer".format(
+                self.vectorizer_guid))
+        self.vectorizers[
+            "item_description"] = "data/light_gbm/{}/item_description_vectorizer/vectorizer.pkl".format(
+                self.vectorizer_guid)
 
         # Convert each feature (brand_name, item_condition_id, shipping) to one-hot-encoded sparse matrix
         # Convert each feature (cat_dae, cat_jung, cat_so) to one-hot-encoded spare matrix
@@ -100,9 +97,12 @@ class LightGBMModel(BaseEstimator, RegressorMixin):
                 "cat_jung", "cat_so"
         ]:
             print("Vectorizing {}".format(col))
-            self.vectorizers[col] = fit_and_save_vectorizer(
-                mercari_df[col], LabelBinarizer(sparse_output=True),
-                "data/light_gbm/{}_vectorizer".format(col))
+            save_path = "data/light_gbm/{}/{}_vectorizer".format(
+                self.vectorizer_guid, col)
+            fit_and_save_vectorizer(df[col],
+                                    LabelBinarizer(sparse_output=True),
+                                    save_path)
+            self.vectorizers[col] = "{}/vectorizer.pkl".format(save_path)
 
     def common_preprocessing(self, X):
         # Calls split_cat() function above and create cat_dae, cat_jung, cat_so columns in X
@@ -123,15 +123,12 @@ class LightGBMModel(BaseEstimator, RegressorMixin):
 
     def apply_preprocessing(self, X):
         X = self.common_preprocessing(X)
-        # X['price'] = np.log1p(X['price'])
 
         sparse_matrix_list = []
         for col, v in self.vectorizers.items():
             with open(v, 'rb') as f:
                 vectorizer = pickle.load(f)
                 sparse_matrix_list.append(vectorizer.transform(X[col]))
-                del vectorizer
 
         X = hstack(sparse_matrix_list).tocsr()
-        # y = X['price']
         return X

code/nn_model.py
@@ -1,6 +1,5 @@
-import math
 import pickle
-from os.path import exists
+from uuid import uuid4
 
 import mlflow
 import numpy as np
@@ -9,6 +8,9 @@ from keras import backend as K
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.layers import (GRU, Dense, Dropout, Embedding, Flatten, Input,
                           concatenate)
+from keras.metrics import (CosineSimilarity, LogCoshError, MeanAbsoluteError,
+                           MeanAbsolutePercentageError, MeanSquaredError,
+                           MeanSquaredLogarithmicError, RootMeanSquaredError)
 from keras.models import Model
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
@@ -16,18 +18,10 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics import make_scorer
 from sklearn.model_selection import RepeatedKFold
 from sklearn.preprocessing import LabelEncoder
-from keras.metrics import LogCoshError, CosineSimilarity, MeanSquaredLogarithmicError, MeanAbsolutePercentageError, MeanAbsoluteError, RootMeanSquaredError, MeanSquaredError
 
 from preprocessing import fit_and_save_vectorizer
-from train import rmsle
 
 pd.set_option('max_colwidth', 200)
-
-# def rmsle(y, y_pred):
-#     assert len(y) == len(y_pred)
-#     to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1))**2.0
-#               for i, pred in enumerate(y_pred)]
-#     return (sum(to_sum) * (1.0 / len(y)))**0.5
 
 
 def get_callbacks(filepath, patience=2):
@@ -48,28 +42,8 @@ class NNModel(BaseEstimator, RegressorMixin):
         self.model = self.get_model(MAX_TEXT, MAX_CATEGORY, MAX_BRAND,
                                     MAX_CONDITION)
         self.vectorizers = {}
-        for col in [
-                "category_name", "brand_name", "item_description_and_name"
-        ]:
-            if not exists("data/nn/{}_vectorizer/vectorizer.pkl".format(col)):
-                self.preprocess()
-
-            with open("data/nn/{}_vectorizer/vectorizer.pkl".format(col),
-                      'rb') as f:
-                self.vectorizers[col] = pickle.load(f)
-
         self.eval_metric = make_scorer(self.score, greater_is_better=False)
-        self.metrics = {
-            "Root mean squared log error": self.eval_metric,
-            "Explained variance": "explained_variance",
-            "Max error": "max_error",
-            "Negative mean absolute error": "neg_mean_absolute_error",
-            "Negative mean squared error": "neg_mean_squared_error",
-            "Negative root mean squared error": "neg_root_mean_squared_error",
-            "Negative mean squared log error": "neg_mean_squared_log_error",
-            "Negitive median absolute error": "neg_median_absolute_error",
-            "R2": "r2"
-        }
+        self.vectorizer_guid = str(uuid4()).replace('-', '_')
 
     #KERAS MODEL DEFINITION
     def get_model(self, MAX_TEXT, MAX_CATEGORY, MAX_BRAND, MAX_CONDITION):
@@ -78,14 +52,11 @@ class NNModel(BaseEstimator, RegressorMixin):
 
         #Inputs
         name = Input(shape=[10], name="name")
-        # name = Input(shape=[X_train["name"].shape[1]], name="name")
         item_desc = Input(shape=[75], name="item_desc")
-        # item_desc = Input(shape=[X_train["item_desc"].shape[1]], name="item_desc")
         brand_name = Input(shape=[1], name="brand_name")
         category_name = Input(shape=[1], name="category_name")
         item_condition = Input(shape=[1], name="item_condition")
         num_vars = Input(shape=[1], name="num_vars")
-        # num_vars = Input(shape=[X_train["num_vars"].shape[1]], name="num_vars")
 
         #Embeddings layers
         emb_name = Embedding(MAX_TEXT, 50)(name)
@@ -131,28 +102,17 @@ class NNModel(BaseEstimator, RegressorMixin):
         return model
 
     def fit(self, X, y):
+        self.vectorizer_guid = str(uuid4()).replace('-', '_')
+        Xp = X.copy(deep=True)
+        MAX_TEXT, MAX_CATEGORY, MAX_BRAND, MAX_CONDITION = self.preprocess(Xp)
+        del Xp
+        self.model = self.get_model(MAX_TEXT, MAX_CATEGORY, MAX_BRAND,
+                                    MAX_CONDITION)
         BATCH_SIZE = 20000
         epochs = 5
         X = self.apply_preprocessing(X)
         y = np.log1p(y)
-        self.model.fit(
-            X,
-            y,
-            epochs=epochs,
-            batch_size=BATCH_SIZE,
-            #    validation_data=(X_valid, y_valid),
-            verbose=1)
-
-        # #EVLUEATE THE MODEL ON DEV TEST: What is it doing?
-        # val_preds = model.predict(X_valid)
-        # #val_preds = target_scaler.inverse_transform(val_preds)
-        # val_preds = np.exp(val_preds) + 1
-
-        # #mean_absolute_error, mean_squared_log_error
-        # y_true = np.array(dvalid.price.values)
-        # y_pred = val_preds[:, 0]
-        # v_rmsle = rmsle(y_true, y_pred)
-        # print(" RMSLE error on dev test: " + str(v_rmsle))
+        self.model.fit(X, y, epochs=epochs, batch_size=BATCH_SIZE, verbose=1)
 
     def predict(self, X):
         X = self.apply_preprocessing(X)
@@ -172,25 +132,15 @@ class NNModel(BaseEstimator, RegressorMixin):
     def get_params(self, deep=True):
         return self.model.get_config()
 
-    def reset_model(
-        self,
-        MAX_TEXT=259088,
-        MAX_CATEGORY=1311,
-        MAX_BRAND=5290,
-        MAX_CONDITION=6,
-    ):
-        self.model = self.get_model(MAX_TEXT, MAX_CATEGORY, MAX_BRAND,
-                                    MAX_CONDITION)
-
     def common_preprocessing(self, dataset):
         dataset.category_name.fillna(value="missing", inplace=True)
         dataset.brand_name.fillna(value="missing", inplace=True)
         dataset.item_description.fillna(value="missing", inplace=True)
         return (dataset)
 
-    def preprocess(self, include_analysis=True):
+    def preprocess(self, X, include_analysis=True):
         print("Loading data...")
-        train = pd.read_table("data/train.tsv")
+        train = X
         test = pd.read_table("data/test.tsv")
         if include_analysis:
             print(train.shape)
@@ -206,40 +156,44 @@ class NNModel(BaseEstimator, RegressorMixin):
 
         #PROCESS CATEGORICAL DATA
         print("Handling categorical variables...")
-
         le = fit_and_save_vectorizer(
-            np.hstack([train.category_name, test.category_name]),
-            LabelEncoder(), "data/nn/category_name_vectorizer")
+            np.hstack([train.category_name,
+                       test.category_name]), LabelEncoder(),
+            "data/nn/{}/category_name_vectorizer".format(self.vectorizer_guid))
+        self.vectorizers[
+            "category_name"] = "data/nn/{}/category_name_vectorizer/vectorizer.pkl".format(
+                self.vectorizer_guid)
+
         train.category_name = le.transform(train.category_name)
         test.category_name = le.transform(test.category_name)
 
         le = fit_and_save_vectorizer(
             np.hstack([train.brand_name, test.brand_name]), LabelEncoder(),
-            "data/nn/brand_name_vectorizer")
+            "data/nn/{}/brand_name_vectorizer".format(self.vectorizer_guid))
+        self.vectorizers[
+            "brand_name"] = "data/nn/{}/brand_name_vectorizer/vectorizer.pkl".format(
+                self.vectorizer_guid)
 
         train.brand_name = le.transform(train.brand_name)
         test.brand_name = le.transform(test.brand_name)
         del le
 
-        # train.head(3)
-
         #PROCESS TEXT: RAW
         print("Text to seq process...")
-
-        # raw_text = np.hstack(
-        #     [train.item_description.str.lower(),
-        #      train.name.str.lower()])
-
         print("   Fitting tokenizer...")
-        # tok_raw = Tokenizer()
-        # tok_raw.fit_on_texts(raw_text)
+
         tok_raw = fit_and_save_vectorizer(
             np.hstack(
                 [train.item_description.str.lower(),
                  train.name.str.lower()]),
             Tokenizer(),
-            "data/nn/item_description_and_name_vectorizer",
+            "data/nn/{}/item_description_and_name_vectorizer".format(
+                self.vectorizer_guid),
             non_sklearn=True)
+        self.vectorizers[
+            "item_description_and_name"] = "data/nn/{}/item_description_and_name_vectorizer/vectorizer.pkl".format(
+                self.vectorizer_guid)
+
         print("   Transforming text to seq...")
 
         train["seq_item_description"] = tok_raw.texts_to_sequences(
@@ -248,7 +202,6 @@ class NNModel(BaseEstimator, RegressorMixin):
             test.item_description.str.lower())
         train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
         test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
-        # train.head(3)
 
         #SEQUENCES VARIABLES ANALYSIS
         if include_analysis:
@@ -267,7 +220,6 @@ class NNModel(BaseEstimator, RegressorMixin):
             train.seq_item_description.apply(lambda x: len(x)).hist()
 
         #EMBEDDINGS MAX VALUE
-        #Base on the histograms, we select the next lengths
         MAX_NAME_SEQ = 10
         MAX_ITEM_DESC_SEQ = 75
         MAX_TEXT = np.max([
@@ -289,11 +241,6 @@ class NNModel(BaseEstimator, RegressorMixin):
             print("MAX_CATEGORY: {}".format(MAX_CATEGORY))
             print("MAX_BRAND: {}".format(MAX_BRAND))
             print("MAX_CONDITION: {}".format(MAX_CONDITION))
-
-        #SCALE target variable
-        # train["target"] = np.log(train.price + 1)
-        # target_scaler = MinMaxScaler(feature_range=(-1, 1))
-        #train["target"] = target_scaler.fit_transform(train.target.reshape(-1,1))
 
         train = {
             'name':
@@ -322,60 +269,26 @@ class NNModel(BaseEstimator, RegressorMixin):
 
         if include_analysis:
             print(train)
-            # pd.DataFrame(train["price"]).hist()
 
-        # train.to_csv("data/nn/nn_dataset_train.csv")
         return MAX_TEXT, MAX_CATEGORY, MAX_BRAND, MAX_CONDITION
-
-        #EXTRACT DEVELOPTMENT TEST
-        # dtrain, dvalid = train_test_split(train, random_state=123, train_size=0.99)
-        # print(dtrain.shape)
-        # print(dvalid.shape)
-
-        #KERAS DATA DEFINITION
-
-        # X_train = get_keras_data(dtrain)
-        # X_valid = get_keras_data(dvalid)
-        # X_test = get_keras_data(test)
 
     def apply_preprocessing(self, X, MAX_NAME_SEQ=10, MAX_ITEM_DESC_SEQ=75):
         X = self.common_preprocessing(X)
-
-        X.category_name = self.vectorizers["category_name"].transform(
-            X.category_name)
-        X.brand_name = self.vectorizers["brand_name"].transform(X.brand_name)
-
-        # X.category_name = load_and_apply_vectorizer(
-        #     "data/nn/category_name_label_encoder/vectorizer.pkl",
-        #     X.category_name)
-        # X.brand_name = load_and_apply_vectorizer(
-        #     "data/nn/brand_name_label_encoder/vectorizer.pkl", X.brand_name)
-        # train.brand_name = le.transform(train.category_name)
-        # train.brand_name = le.transform(train.brand_name)
-
-        self.vectorizers["item_description_and_name"].texts_to_sequences(
-            X.item_description.str.lower())
-
-        self.vectorizers["item_description_and_name"].texts_to_sequences(
-            X.name.str.lower())
-
-        with open(
-                "data/nn/item_description_and_name_vectorizer/vectorizer.pkl",
-                'rb') as f:
+        print(self.vectorizers["category_name"])
+        with open(self.vectorizers["category_name"], 'rb') as f:
             vectorizer = pickle.load(f)
+            X.category_name = vectorizer.transform(X.category_name)
 
-        X["seq_item_description"] = vectorizer.texts_to_sequences(
-            X.item_description.str.lower())
-        X["seq_name"] = vectorizer.texts_to_sequences(X.name.str.lower())
-        # train["seq_item_description"] = tok_raw.texts_to_sequences(
-        #     train.item_description.str.lower())
-        # test["seq_item_description"] = tok_raw.texts_to_sequences(
-        #     test.item_description.str.lower())
-        # train["seq_name"] = tok_raw.texts_to_sequences(train.name.str.lower())
-        # test["seq_name"] = tok_raw.texts_to_sequences(test.name.str.lower())
+        with open(self.vectorizers["brand_name"], 'rb') as f:
+            vectorizer = pickle.load(f)
+            X.brand_name = vectorizer.transform(X.brand_name)
 
-        # y = np.log(X.price + 1)
-        # train["target"] = np.log(train.price + 1)
+        with open(self.vectorizers["item_description_and_name"], 'rb') as f:
+            vectorizer = pickle.load(f)
+            X["seq_item_description"] = vectorizer.texts_to_sequences(
+                X.item_description.str.lower())
+            X["seq_name"] = vectorizer.texts_to_sequences(X.name.str.lower())
+
         X = {
             'name':
             pad_sequences(X.seq_name, maxlen=MAX_NAME_SEQ),
@@ -393,8 +306,7 @@ class NNModel(BaseEstimator, RegressorMixin):
 
         return X
 
-    def my_evaluate(self, X, y, n_splits=10, n_repeats=5, n_jobs=1):
-        n_jobs = 1
+    def my_evaluate(self, X, y, n_splits=10, n_repeats=5):
         experiment_names = [x.name for x in mlflow.list_experiments()]
         if self.experiment not in experiment_names:
             mlflow.create_experiment(self.experiment)
@@ -409,14 +321,8 @@ class NNModel(BaseEstimator, RegressorMixin):
                                random_state=42)
             for train_index, test_index in cv.split(X):
                 print("ITERATION: {}".format(i))
-                self.reset_model()
                 X_train, X_test = X.loc[train_index], X.loc[test_index]
                 y_train, y_test = y.loc[train_index], y.loc[test_index]
-                # X_train, X_test = {k: v[train_index]
-                #                    for k, v in X.items()
-                #                    }, {k: v[test_index]
-                #                        for k, v in X.items()}
-                # y_train, y_test = y[train_index], y[test_index]
 
                 self.fit(X_train, y_train)
                 X_test = self.apply_preprocessing(X_test)
@@ -428,20 +334,3 @@ class NNModel(BaseEstimator, RegressorMixin):
                 results = {"test_{}".format(k): v for k, v in results.items()}
                 mlflow.log_metrics(results)
                 i += 1
-
-            # results = cross_validate(self.model,
-            #                          X,
-            #                          y,
-            #                          scoring=self.metrics,
-            #                          cv=cv,
-            #                          n_jobs=n_jobs,
-            #                          return_estimator=True,
-            #                          verbose=1)
-
-            # results["fit_time"] = [np.double(x) for x in results["fit_time"]]
-            # for k, v in results.items():
-            #     for i in range(len(v)):
-            #         if k == "estimator":
-            #             mlflow.log_params(v[i].get_params(deep=True))
-            #         else:
-            #             mlflow.log_metric(k, v[i])

code/train.py
@@ -1,10 +1,17 @@
+import math
+
 import numpy as np
 import pandas as pd
+from lightgbm import LGBMRegressor
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
 
 from model_logger import run
 
 pd.set_option('max_colwidth', 200)
-model_type = "light_gbm"
+model_type = "NN"
 
 
 def rmsle(y, y_pred):
@@ -12,9 +19,49 @@ def rmsle(y, y_pred):
     return np.sqrt(np.mean(np.power(np.log1p(y) - np.log1p(y_pred), 2)))
 
 
+# import gc
+
+# import scipy.sparse as sp
+# from scipy.sparse import hstack
+
+# def model_train_predict(model, matrix_list_X, matrix_list_Y, df, df_test):
+#     # scipy.sparse 모듈의 hstack 을 이용하여 sparse matrix 결합
+#     #
+#     X = hstack(matrix_list_X).tocsr()
+#     Y = hstack(matrix_list_Y).tocsr()
+#     # X = matrix_list_X
+#     # Y = matrix_list_Y
+
+#     # X_train, X_test, y_train, y_test=train_test_split(X, df['price'],
+#     #                                                   test_size=0.1)
+
+#     # Y_train, Y_test, y_price, y_useless=train_test_split(Y, df_test['price'],
+#     #                                                   test_size=0.1)
+
+#     X_price = df['price']
+#     Y_price = df_test['price']
+
+#     # 모델 학습 및 예측
+#     # Model traiing and test
+#     preds = []
+#     try:
+#         model.fit(X, X_price)
+#         preds = model.predict(Y)
+#     except:
+#         print("ERROR")
+#         preds = [0]
+#         Y_price = [1000000]
+#         np.array(preds)
+#         np.array(Y_price)
+
+#     #del X , X_train , X_test , Y
+#     gc.collect()
+
+#     return preds, Y_price
+
 if __name__ == "__main__":
-    mercari_df = pd.read_csv('data/train.tsv', sep='\t')
-    X = mercari_df[[x for x in mercari_df.columns if x != "price"]]
+    mercari_df = pd.read_csv('data/train.tsv', sep='\t', nrows=10000)
+    X = mercari_df
     y = mercari_df["price"]
 
     print("Preprocessing dataset")
@@ -22,13 +69,187 @@ if __name__ == "__main__":
         from light_gbm_model import LightGBMModel
         model = LightGBMModel()
         run(model, X, y, model.metrics, experiment=model.experiment, n_jobs=1)
-    else:
+    elif model_type == "NN":
         from nn_model import NNModel
         model = NNModel()
         model.my_evaluate(X, y)
-        # model.preprocess()
-        # X, y = model.apply_preprocessing(train)
-        # run(model, X, y, model.metrics, experiment=model.experiment, n_jobs=1)
+    else:
+        from category_model import CategoryModel
+        model = CategoryModel()
+        model.fit(mercari_df)
+
+        # df_train = df.sample(frac=0.8, random_state=25)
+        # df_test = df.drop(df_train.index)
+
+        # df_train['brand_name'] = df_train['brand_name'].fillna(
+        #     value='Other_Null')
+        # df_train['category_name'] = df_train['category_name'].fillna(
+        #     value='Other_Null')
+        # df_train['item_description'] = df_train['item_description'].fillna(
+        #     value='Other_Null')
+
+        # # 각 컬럼별로 Null값 건수 확인. 모두 0가 나와야 합니다.
+        # print(df_train.isnull().sum())
+
+        # df_test['brand_name'] = df_test['brand_name'].fillna(
+        #     value='Other_Null')
+        # df_test['category_name'] = df_test['category_name'].fillna(
+        #     value='Other_Null')
+        # df_test['item_description'] = df_test['item_description'].fillna(
+        #     value='Other_Null')
+
+        # # 각 컬럼별로 Null값 건수 확인. 모두 0가 나와야 합니다.
+        # df_test.isnull().sum()
+
+        # training_grouped = df_train.groupby(df_train.category_name)
+        # testing_grouped = df_test.groupby(df_test.category_name)
+        # print(training_grouped.dtypes)
+        # print(testing_grouped.dtypes)
+
+        # id_array = []
+        # for t in training_grouped.category_name:
+        #     id_array.append(t)
+
+        # print(len(id_array))
+        # print(id_array[0][0])
+        # print(training_grouped.get_group(id_array[0][0]))
+
+        # id_array_testing = []
+        # for t in testing_grouped.category_name:
+        #     id_array_testing.append(t)
+
+        # print(len(id_array_testing))
+        # print(id_array_testing[0][0])
+        # print(testing_grouped.get_group(id_array_testing[0][0]))
+
+        # df_array = {}
+        # for t in id_array:
+        #     df_array[t[0]] = (training_grouped.get_group(t[0]))
+
+        # print(len(df_array))
+        # print(df_array.keys())
+
+        # df_array_testing = {}
+        # for t in id_array_testing:
+        #     df_array_testing[t[0]] = (testing_grouped.get_group(t[0]))
+
+        # print(len(df_array_testing))
+        # print(df_array_testing.keys())
+
+        # targets = []
+        # predictions = []
+        # for category in df_array:
+
+        #     train_cat = ""
+        #     if category in df_array:
+        #         train_cat = category
+        #     else:
+        #         split = category.split('/')
+        #         train_cat = split[0] + '/' + split[1]
+
+        #         for category in df_array:
+        #             if category.find(train_cat) != -1:
+        #                 train_cat = category
+        #                 break
+
+        #     print(train_cat)
+
+        #     try:
+        #         df = pd.DataFrame(df_array[train_cat])
+        #         df_test = pd.DataFrame(df_array_testing[category])
+        #     except:
+        #         continue
+
+        #     combo = pd.concat([df, df_test])
+
+        #     # Convert "name" with feature vectorization
+        #     cnt_vec = CountVectorizer(max_features=30000)
+        #     combo_name = cnt_vec.fit(combo.name)
+        #     X_name = cnt_vec.transform(df.name)
+        #     Y_name = cnt_vec.transform(df_test.name)
+
+        #     # Convert "item_description" with feature vectorization
+        #     tfidf_descp = TfidfVectorizer(max_features=50000,
+        #                                   ngram_range=(1, 3),
+        #                                   stop_words='english')
+        #     combo_desc = tfidf_descp.fit(combo['item_description'])
+        #     X_descp = tfidf_descp.transform(df['item_description'])
+        #     Y_descp = tfidf_descp.transform(df_test['item_description'])
+
+        #     # Convert each feature (brand_name, item_condition_id, shipping) to one-hot-encoded sparse matrix
+        #     lb_brand_name = LabelBinarizer(sparse_output=True)
+        #     combo_brand = lb_brand_name.fit(combo['brand_name'])
+        #     X_brand = lb_brand_name.transform(df['brand_name'])
+        #     Y_brand = lb_brand_name.transform(df_test['brand_name'])
+
+        #     lb_item_cond_id = LabelBinarizer(sparse_output=True)
+        #     combo_item = lb_item_cond_id.fit(combo['item_condition_id'])
+        #     X_item_cond_id = lb_item_cond_id.transform(df['item_condition_id'])
+        #     Y_item_cond_id = lb_item_cond_id.transform(
+        #         df_test['item_condition_id'])
+
+        #     lb_shipping = LabelBinarizer(sparse_output=True)
+        #     combo_ship = lb_shipping.fit(combo['shipping'])
+        #     X_shipping = lb_shipping.transform(df['shipping'])
+        #     Y_shipping = lb_shipping.transform(df_test['shipping'])
+
+        #     # # RMSLE with Item Description
+        #     # linear_model = Ridge(solver = "lsqr", fit_intercept=False)
+        #     sparse_matrix_list_X = [
+        #         X_descp, X_name, X_brand, X_item_cond_id, X_shipping
+        #     ]
+        #     sparse_matrix_list_Y = [
+        #         Y_descp, Y_name, Y_brand, Y_item_cond_id, Y_shipping
+        #     ]
+        #     # linear_preds , y_test = model_train_predict(model=linear_model , matrix_list_X=sparse_matrix_list_X, matrix_list_Y=sparse_matrix_list_Y, df=df, df_test=df_test)
+
+        #     lgbm_model = LGBMRegressor(n_estimators=50,
+        #                                learning_rate=0.5,
+        #                                num_leaves=125,
+        #                                random_state=156)
+        #     lgbm_preds, y_test = model_train_predict(
+        #         model=lgbm_model,
+        #         matrix_list_X=sparse_matrix_list_X,
+        #         matrix_list_Y=sparse_matrix_list_Y,
+        #         df=df,
+        #         df_test=df_test)
+
+        #     try:
+        #         lgbm_preds = abs(lgbm_preds)
+
+        #         # Compute median for this group and see if that produces a better error. If so use that.
+        #         # median = math.floor(df['price'].median())
+        #         # medians = []
+        #         # for i in y_test:
+        #         #     medians.append(median)
+        #         # medians = np.array(medians)
+
+        #         model_error = np.sqrt(
+        #             np.mean(
+        #                 np.power(np.log1p(lgbm_preds) - np.log1p(y_test), 2)))
+        #         #median_error = np.sqrt(np.mean(np.power(np.log1p(medians) - np.log1p(y_test), 2)))
+        #         print("Model ", model_error)
+        #         #print("Median ", median_error)
+
+        #         #if model_error < median_error:
+        #         for pred in lgbm_preds:
+        #             predictions.append(pred)
+        #         # else:
+        #         #     for median in medians:
+        #         #         predictions.append(median)
+
+        #         for target in y_test:
+        #             targets.append(target)
+        #     except:
+        #         print("ERROR")
+
+        # targets = np.array(targets)
+        # predictions = np.array(predictions)
+        # predictions = np.abs(predictions)
+        # print(
+        #     np.sqrt(
+        #         np.mean(np.power(np.log1p(predictions) - np.log1p(targets),
+        #                          2))))
 
     # model.preprocess()
 